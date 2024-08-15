8f83b605ca42ab3e354afb7a9067833b280bd86a
Keneziak <68872928+Keneziak@users.noreply.github.com>
2024-08-14 21:17:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const dropdown = document.querySelector('.dropdown');
     const dropdownContent = document.querySelector('.dropdown-content');
 
-    dropdownContent.style.display = 'none';
+    dropdownContent.style.display = 'none'; // Asegurarse de que el menú esté oculto al cargar la página
 
     dropdown.addEventListener('mouseover', function() {
         dropdownContent.style.display = 'flex';
