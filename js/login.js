bd1e780f5a171069947ea5579c9eb2bb8ea19281
DaniloSimone <135861539+DaniloSimone@users.noreply.github.com>
2024-08-16 19:20:31 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -35,13 +35,13 @@ let forml = document.querySelector('.login');
       let nombre = document.querySelector('.loginnombre').value;
       let contrasena = document.querySelector('.logincontrasena').value; 
       const login = async ()=>{
-        let request = await fetch(`php/register.php`, {
+        let request = await fetch(`php/login.php`, {
           headers: {
             'Content-Type': 'application/json',
           },
           method: "POST",
           body: JSON.stringify({
-            nombre, mail, contrasena
+            nombre, contrasena
           }),
           credentials: 'include'  
         });
@@ -54,8 +54,6 @@ let forml = document.querySelector('.login');
         }
         localStorage.setItem("usuariosesion", response);
         console.log(localStorage.getItem("usuariosesion"));
-        document.location.href = "./index.html"
-        
     }
     login()
 })
